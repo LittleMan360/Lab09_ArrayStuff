50d9658e4a67cdfd703063fb8fa926f1e5819431
Dalton <dalton.m.towe@gmail.com>
2023-03-26 22:12:46 -0400

    code written works and commented

--- src/DataPoints.java
+++ src/DataPoints.java
@@ -1,10 +1,102 @@
+import java.util.Random;
+import java.util.Scanner;
 
 public class DataPoints {
 
     public static void main(String[] args)
     {
+        Scanner in = new Scanner(System.in);
 
 
+
+        int firstMatchingIndex = -1;
+        int inputMatchIndex;
+        int inputNumber ;
+        final int COUNT = 100;
+        int[] dataPoints = new int[COUNT];
+        Random r = new Random();
+        for(int i = 0 ; i < COUNT; i++){
+//populate int between 1 and 100
+            dataPoints[i] = 1+r.nextInt(100);
+        }
+//print numbers separating with pipe
+        for(int i= 0; i < COUNT; i++){
+            int currentDataPoint = dataPoints[i];
+            System.out.print(currentDataPoint);
+            if(i < COUNT-1)
+                System.out.print("|");
+        }
+
+        int sum = 0;
+//do sum
+        for(int i = 0 ; i < COUNT; i++){
+            sum = sum + dataPoints[i];
+        }
+//do average
+        double avg = (float)sum/COUNT;
+//print sum and average
+        System.out.println("\nThe SUM of the random array dataPoints is: "+sum);
+        System.out.println("The AVERAGE of the random array dataPoints is: "+avg);
+
+
+//get input between 1 and 100
+        inputNumber = SafeInput.getRangedInt(in, "Enter a number between 1 and 100", 1, 100);
+//find total match of input in dataPoints
+        int matchCount = 0;
+        for(int i : dataPoints){
+            if(i==inputNumber)
+                matchCount++;
+        }
+//print
+        System.out.println("INPUT "+inputNumber+" is found "+matchCount+" times in the dataPoints array.");
+
+//get input in
+
+        inputMatchIndex = SafeInput.getRangedInt(in, "Enter another number between 1 and 100", 1, 100);
+//find first matching index in dataPoints
+        for(int i = 0; i < COUNT; i++){
+            if(dataPoints[i]==inputMatchIndex){
+                firstMatchingIndex = i;
+                break;
+            }
+        }
+        if(firstMatchingIndex == -1){
+            System.out.println("The value "+inputMatchIndex+" was NOT FOUND at dataPoints array.");
+        }else{
+            System.out.println("The value "+inputMatchIndex+" was found at array index "+firstMatchingIndex);
+        }
+
+
+        int min = dataPoints[0];
+        int max = dataPoints[0];
+//fins minimum and maximum
+        for(int i = 1; i < COUNT; i++){
+            int currentData= dataPoints[i];
+            if(currentData > max)
+                max = currentData;
+            if(currentData < min)
+                min = currentData;
+        }
+
+        System.out.println("Minimum Value in the dataPoints = "+min);
+        System.out.println("Maximum Value in the dataPoints = "+max);
+//define double array
+        double[] dataPointsDouble = {5.89,20.67,84.22,122.99,265.90,12.75};
+//call getAverage
+        System.out.println("Average of dataPoints(double) is: " + getAverage(dataPointsDouble));
+
+    }
+
+    public static double getAverage(double[] values){
+        double sum = 0;
+        double avg = 0;
+        if(values!=null && values.length!=0){
+            for(double d: values){
+                sum = sum + d;
+            }
+            avg = sum/values.length;
+        }
+        return avg;
 
     }
 }
--- src/SafeInput.java
+++ src/SafeInput.java
@@ -1,67 +1,278 @@
+
 import java.util.Scanner;
 
-public class SafeInput {
-    public static String getNonZeroLenString (Scanner pipe , String prompt) {
-        System.out.println(prompt);
-        String input = pipe.nextLine();
-        while(input.length() == 0) {
-            System.out.println("Cannot be empty");
-            System.out.println(prompt);
-            input = pipe.nextLine();
+public class SafeInput 
+{
+
+   public static String getNonZeroLenString(Scanner pipe, String prompt)
+   {
+       String result = " ";
+       boolean done = false;
+       
+       do
+       {
+           System.out.print("\n" + prompt + ": ");
+           result = pipe.nextLine();
+           if(result.length() >= 1)
+               done = true;
+           else
+               System.out.println("\nYou must enter at least one character!\nTry again.");
+           
+       }while(!done);
+       return result;
+       
+   }
+
+    public static int getRangedInt(Scanner pipe, String prompt, int low, int high)
+    {
+       int retVal = 0;
+       String trash = "";
+       boolean done = false;
+       
+       do
+       {
+           System.out.print("\n" + prompt + "[" + low + "-" + high + "]: ");
+           if(pipe.hasNextInt())
+           {
+               retVal = pipe.nextInt();
+               pipe.nextLine();
+               if(retVal >= low && retVal <= high)
+               {
+                  done = true;
+               }
+               else
+               {
+                   System.out.println("\nNumber is out of range [" + low + "-" + high + "]: " + retVal);
+               }
+           }
+           else
+           {
+               trash = pipe.nextLine();
+               System.out.println("You must enter an int: " + trash);
+           }
+       }while(!done);
+       
+       return retVal;
+    }
+
+    public static int getInt(Scanner pipe, String prompt)
+    {
+       int retVal = 0;
+       String trash = "";
+       boolean done = false;
+       
+       do
+       {
+           System.out.print("\n" + prompt + ": ");
+           if(pipe.hasNextInt())
+           {
+               retVal = pipe.nextInt();
+               pipe.nextLine();
+               done = true;               
+           }
+           else
+           {
+               trash = pipe.nextLine();
+               System.out.println("You must enter an int: " + trash);
+           }
+       }while(!done);
+       
+       return retVal;
+    }
+
+    
+
+    public static double getRangedDouble(Scanner pipe, String prompt, double low, double high)
+    {
+       double retVal = 0;
+       String trash = "";
+       boolean done = false;
+       
+       do
+       {
+           System.out.print("\n" + prompt + "[" + low + "-" + high + "]: ");
+           if(pipe.hasNextDouble())
+           {
+               retVal = pipe.nextDouble();
+               pipe.nextLine();
+               if(retVal >= low && retVal <= high)
+               {
+                  done = true;
+               }
+               else
+               {
+                   System.out.println("\nNumber is out of range [" + low + "-" + high + "]: " + retVal);
+               }
+           }
+           else
+           {
+               trash = pipe.nextLine();
+               System.out.println("You must enter a double: " + trash);
+           }
+       }while(!done);
+       
+       return retVal;
+    } 
+    
+
+    public static double getDouble(Scanner pipe, String prompt)
+    {
+       double retVal = 0;
+       String trash = "";
+       boolean done = false;
+       
+       do
+       {
+           System.out.print("\n" + prompt + ": ");
+           if(pipe.hasNextDouble())
+           {
+               retVal = pipe.nextDouble();
+               pipe.nextLine();
+               done = true;
+           }
+           else
+           {
+               trash = pipe.nextLine();
+               System.out.println("You must enter a double: " + trash);
+           }
+       }while(!done);
+       
+       return retVal;
+    }     
+    
+
+    public static boolean getYNConfirm(Scanner pipe, String prompt)
+    {
+        boolean retVal = true;
+        String response = "";
+        boolean gotAVal = false;
+        
+        do
+        {
+            System.out.print("\n" + prompt + " [Y/N] ");
+            response = pipe.nextLine();
+            if(response.equalsIgnoreCase("Y"))
+            {
+                gotAVal = true;
+                retVal = true;
+            }
+            else if(response.equalsIgnoreCase("N"))
+            {
+                gotAVal = true;
+                retVal = false;
+            }
+            else
+            {
+                System.out.println("You must answer [Y/N]! " + response );
+            }
+            
+        }while(!gotAVal);
+        
+        return retVal;
+    }
+
+    
+    public static String getRegExString(Scanner pipe, String prompt, String regExPattern)
+    {
+        String response = "";
+        boolean gotAVal = false;
+        
+        do
+        {
+            System.out.print("\n" + prompt + ": ");
+            response = pipe.nextLine();
+            if(response.matches(regExPattern))
+            {
+                gotAVal = true;                
+            }
+            else
+            {
+                System.out.println("\n" + response + " must match the pattern " + regExPattern);
+                System.out.println("Try again!");
+            } 
+            
+        }while(!gotAVal);
+        
+        return response;
+    }
+    public static void prettyHeader(String msg)
+   {
+       boolean done = false;
+       int lineLength = 60;
+       int asterisks = 3;
+       int spacesAroundName = ((lineLength - msg.length()) / 2);
+       
+       do
+       {
+           if(msg.length() >= 1 && msg.length() <= 60)
+           {
+               done = true;
+           }
+               
+           else
+           {
+               System.out.println("\nYour title must be between 1-60 characters!\nTry again.");
+               done = false;
+           }
+
+       }while(!done);
+       
+       for(int i = 1; i <= 65; i++)
+        {
+            for(int j = 1; j <= 1; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.print("");
         }
-        return input;
-    }
-
-    public static int getInt(Scanner pipe , String prompt) {
-        System.out.println(prompt);
-        int input = Integer.parseInt(pipe.nextLine());  // using  integer.parseInt to fix newline issue. No need to hasNext in this approach
-        return input;
-    }
-
-    public static double getDouble(Scanner pipe , String prompt) {
-        System.out.println(prompt);
-        double input = Double.parseDouble(pipe.nextLine());  // using  Double.parseDouble to fix newline issue. No need to hasNext in this approach
-        return input;
-    }
-
-    public static int getRangeInt(Scanner pipe , String prompt , int low , int high) {
-        System.out.println(prompt + " between "+low+" and "+high);
-        int input = Integer.parseInt(pipe.nextLine());  // using  integer.parseInt to fix newline issue. No need to hasNext in this approach
-        while(input<low || input >high) {
-            System.out.println("Please enter between the range");
-            System.out.println(prompt + " between "+low+" and "+high);
-            input = Integer.parseInt(pipe.nextLine());
+            
+        for(int i = 1; i <= 1; i++)
+        {
+            for(int j = 1; j <= 1; j++)
+            {
+                System.out.print("\n***");
+            }
+            System.out.print("");
         }
 
-        return input;
-    }
-
-    public static double getRangeDouble(Scanner pipe , String prompt , double low , double high) {
-        System.out.println(prompt + " between "+low+" and "+high);
-        double input = Double.parseDouble(pipe.nextLine());  // using  Double.parseDouble to fix newline issue. No need to hasNext in this approach
-        while(input<low || input >high) {
-            System.out.println("Please enter between the range");
-            System.out.println(prompt + " between "+low+" and "+high);
-            input = Double.parseDouble(pipe.nextLine());
+        printSpacesAroundName(spacesAroundName);
+        System.out.print(msg);
+        printSpacesAroundName(spacesAroundName);
+        
+        for(int i = 62; i <= 62; i++)
+        {
+            for(int j = 62; j <= 62; j++)
+            {
+                System.out.print("***");
+            }
+            System.out.print("\n");
         }
-        return input;
-    }
-
-    public static boolean getYNConfirm(Scanner pipe , String prompt) {
-        System.out.println(prompt);
-        String input = pipe.nextLine();
-        while(!input.equalsIgnoreCase("n") && !input.equalsIgnoreCase("y") ) {
-            System.out.println("Please enter either n N or y Y");
-            System.out.println(prompt);
-            input = pipe.nextLine();
+        
+        for(int i = 1; i <= 65; i++)
+        {
+            for(int j = 3; j <= 3; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.print("");
         }
-        if(input.equalsIgnoreCase("n")) {
-            return false;
-        }else {
-            return true;
-        }
+
+   }
+   private static void printSpacesAroundName(int spacesAroundName) 
+    {
+        for (int i = 0; i < spacesAroundName; i++)
+            System.out.print(" ");
     }
-
-
-
+   
+   public static double getAverage(int values[])
+   {
+        int sum = 0;
+        double ave = 0;
+        for(int r = 0; r < values.length; r++)
+        {
+            sum+= values[r];
+        }
+        ave = sum / values.length;
+   return ave;    
+   } 
 }
